5f133c1bae80eac730207e724be3437eb7f94229
sai-gcdev <sai.gcdev@gmail.com>
2025-05-06 10:01:03 +0530

    Create wishlist.js

---
+++ js/wishlist.js
@@ -0,0 +1,14 @@
+const wishlistGrid = document.getElementById("wishlistGrid");
+
+function getWishlistItems() {
+  const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+  return products.filter((product) => wishlist.includes(product.id));
+}
+
+function displayWishlist() {
+  const items = getWishlistItems();
+  wishlistGrid.innerHTML = "";
+
+  if
+::contentReference[oaicite:0]{index=0}
+ 
